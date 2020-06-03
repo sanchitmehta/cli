71a55913f5192ab39e8104b2b82578495ba00354
Sanchit Mehta <sanchit.mehta602@gmail.com>
2020-06-03 12:40:05 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as path from 'path';
 
 import { createScriptFile, TEMP_DIRECTORY, NullOutstreamStringWritable, deleteFile, getCurrentTime, checkIfEnvironmentVariableIsOmitted } from './utils';
 
-const START_SCRIPT_EXECUTION_MARKER: string = `Starting script execution via docker image mcr.microsoft.com/azure-cli:`;
+const START_SCRIPT_EXECUTION_MARKER: string = `Starting script execution via docker image sanchitm/azure-cli:latest:`;
 const BASH_ARG: string = `bash --noprofile --norc -e `;
 const CONTAINER_WORKSPACE: string = '/github/workspace';
 const CONTAINER_TEMP_DIRECTORY: string = '/_temp';
@@ -53,7 +53,7 @@ const run = async () => {
         command += ` ${environmentVariables} `;
         command += `-e GITHUB_WORKSPACE=${CONTAINER_WORKSPACE} `;
         command += `--name ${CONTAINER_NAME} `;
-        command += ` mcr.microsoft.com/azure-cli:${azcliversion} ${startCommand}`;
+        command += ` sanchitm/azure-cli:latest ${startCommand}`;
         console.log(`${START_SCRIPT_EXECUTION_MARKER}${azcliversion}`);
         await executeDockerCommand(command);
         console.log("az script ran successfully.");
@@ -71,11 +71,7 @@ const run = async () => {
 };
 
 const checkIfValidCLIVersion = async (azcliversion: string): Promise<boolean> => {
-    const allVersions: Array<string> = await getAllAzCliVersions();
-    if (!allVersions || allVersions.length == 0) {
-        return true;
-    }
-    return allVersions.some((eachVersion) => eachVersion.toLowerCase() === azcliversion);
+    return true;
 }
 
 const getAllAzCliVersions = async (): Promise<Array<string>> => {
